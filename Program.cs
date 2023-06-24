4c8de45b152730cca6c77e58e376505c629c6e74
Ethan M. Lee <ethan@mlee.xyz>
2023-06-23 20:09:12 -0400

    init

--- Program.cs
+++ Program.cs
@@ -1,14 +1,3 @@
-﻿using System;
-
-namespace monogame_project
-{
-    public static class Program
-    {
-        [STAThread]
-        static void Main()
-        {
-            using (var game = new Game1())
-                game.Run();
-        }
-    }
-}
+﻿
+using var game = new monogame_project.Game1();
+game.Run();
